ab8852618991e9d14b65e5e1f28192167b03d257
Anton <117667360+rlnq@users.noreply.github.com>
2023-01-21 14:33:48 +0200

    Update startup.groovy

--- jenkins-home/init.groovy.d/startup.groovy
+++ jenkins-home/init.groovy.d/startup.groovy
@@ -1,40 +1,14 @@
 import jenkins.model.*
-import hudson.model.*
-import java.util.logging.Logger
+import hudson.security.*
 
-Logger.global.info("[Running] startup script")
-
-configureSecurity()
-
-Jenkins.instance.save()
-
-buildJob('an-example-of-github-project')
-
-Logger.global.info("[Done] startup script")
-
-#private void configureSecurity() {
-#    Jenkins.getInstance().disableSecurity()
-#}
-
-#user
 def instance = Jenkins.getInstance()
 
 def hudsonRealm = new HudsonPrivateSecurityRealm(false)
 hudsonRealm.createAccount("MyUSERNAME","MyPASSWORD")
 instance.setSecurityRealm(hudsonRealm)
-def strategy = new GlobalMatrixAuthorizationStrategy()
-strategy.add(Jenkins.ADMINISTER, "myUSERNAME")
-instance.setAuthorizationStrategy(strategy)
-instance.save()
-#end user
 
-private def buildJob(String jobName) {
-    Logger.global.info("Building job '$jobName")
-    def job = Jenkins.instance.getJob(jobName)
-    Jenkins.instance.queue.schedule(job, 0, new CauseAction(new Cause() {
-        @Override
-        String getShortDescription() {
-            'Jenkins startup script'
-        }
-    }))
-}
+def strategy = new hudson.security.FullControlOnceLoggedInAuthorizationStrategy()
+strategy.setAllowAnonymousRead(false)
+instance.setAuthorizationStrategy(strategy)
+
+instance.save()
